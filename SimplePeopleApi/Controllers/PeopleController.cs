a3b44b8093f3c18bea29548fe93bb196c9ce1ef1
Sejal Gheenah <sejalgheenah@gmail.com>
2025-11-11 08:04:29 +0400

    People App

---
+++ SimplePeopleApi/Controllers/PeopleController.cs
@@ -0,0 +1,73 @@
+﻿using SimplePeopleApi.Data;
+using SimplePeopleApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimplePeopleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PeopleController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Person>>> GetPeople()
+        {
+            return Ok(await _context.People.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route(("{id}"))]
+        public async Task<ActionResult<List<Person>>> GetPerson(int id)
+        {
+            return Ok(await _context.People.ToListAsync());
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<ActionResult<List<Person>>> CreatePerson(Person person)
+        {
+            _context.People.Add(person);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.People.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Person>>> UpdatePerson(Person person)
+        {
+            var dbPerson = await _context.People.FindAsync(person.Id);
+            if (dbPerson == null)
+                return BadRequest("Person not found.");
+
+            dbPerson.FirstName = person.FirstName;
+            dbPerson.LastName = person.LastName;
+            dbPerson.Email = person.Email;
+            dbPerson.PhoneNumber = person.PhoneNumber;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.People.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Person>>> DeletePerson(int id)
+        {
+            var dbPerson = await _context.People.FindAsync(id);
+            if (dbPerson == null)
+                return BadRequest("Person not found.");
+
+            _context.People.Remove(dbPerson);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.People.ToListAsync());
+        }
+    }
+}
---
+++ SimplePeopleApi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplePeopleApi.Models;
+
+namespace SimplePeopleApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Person> People => Set<Person>();
+    }
+}
